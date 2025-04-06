590d52cb39e9222a036f9525bd20d08a573db46f
Malak Al-Aabiad <144251671+Malak-Alaabiad@users.noreply.github.com>
2025-04-06 22:12:50 +0200

    Update StatisticalCalculation.h

--- problem_five/StatisticalCalculation.h
+++ problem_five/StatisticalCalculation.h
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string>
+
 template <typename T>
-class StatisticalCalculation{
+class StatisticalCalculation {
 private:
     T* data;
     int size;
-    string fileName;
+    std::string fileName;
 public:
     StatisticalCalculation(int size);
     ~StatisticalCalculation();
@@ -17,6 +19,5 @@ public:
     void displayArray();
     void inputData();
     void statisticsMenu();
-    void setFileName(string& fileName);
-
+    void setFileName(const std::string& fileName);
 };
