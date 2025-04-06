6feb35027ff185a351765383acf2de54427c2385
AbdoSaber2 <161854751+AbdoSaber2@users.noreply.github.com>
2025-04-06 19:55:55 +0200

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -97,70 +97,53 @@ float *subtract(float *a, float *b, int size_a, int size_b)
 }
 int main()
 {
-    while (1)
+    cout << "intput format :\n"
+         << "1 - first line take the name of the file\n"
+         << "2 - then enter the order of first polynomial\n"
+         << "3 - then enter the coefficients of first polynomial start from constant term to x^n\n"
+         << "4 - then enter the order of second polynomial\n"
+         << "5 - then enter the coefficients of second polynomial\n"
+         << "output in the file output.txt\n";
+    cout << "1 ---> from terminal\n"
+         << "2,3,4,5 ---> from file\n";
+    string s;
+    cout << "enter the name of the file : ";
+    cin >> s;
+    freopen(s.c_str(), "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n;
+    cin >> n;
+    float consta = 0;
+    float *a = new float[n + 1];
+    cin >> consta;
+    for (int i = 0; i < n + 1; i++)
     {
-        cout << "1 - EXIT\n2 - start a program\n";
-        int choice;
-        cin >> choice;
-        if (choice == 1)
-        {
-            break;
-        }
-        cout << "enter order of first polynomial : ";
-        int n;
-        cin >> n;
-        float consta = 0;
-        float *a = new float[n + 1];
-        cout << "enter coefficients of first polynomial : ";
-        cin >> consta;
-        for (int i = 0; i < n + 1; i++)
-        {
-            cin >> a[i];
-        }
-        cout << "enter order of second polynomial : ";
-        int m;
-        cin >> m;
-        float constb = 0;
-        float *b = new float[m + 1];
-        cout << "enter coefficients of second polynomial : ";
-        cin >> constb;
-        for (int i = 0; i < m + 1; i++)
-        {
-            cin >> b[i];
-        }
-        while (1)
-        {
-            cout << "1 - EXIT\n2 - sum of two polynomial\n3 - difference of two polynomial\n4 - product of two polynomial\n5 - all\n";
-            cin >> choice;
-            if (choice == 1)
-            {
-                break;
-            }
-            float constc = 0;
-            float *c;
-            if (choice == 2 || choice == 5)
-            {
-                cout << "sum of two polynomials : ";
-                c = add(a, b, n + 1, m + 1);
-                constc = consta + constb;
-                print(c, max(n + 1, m + 1), constc);
-            }
-            if (choice == 3 || choice == 5)
-            {
-                cout << "difference of two polynomials : ";
-                constc = constb - consta;
-                c = subtract(b, a, m + 1, n + 1);
-                print(c, max(n + 1, m + 1), constc);
-            }
-            if (choice == 4 || choice == 5)
-            {
-                cout << "product of two polynomials : ";
-                constc = consta * constb;
-                c = multiply(a, b, n + 1, m + 1);
-                print(c, n + 1 + m, constc);
-            }
-        }
-        delete[] a;
-        delete[] b;
+        cin >> a[i];
     }
+    int m;
+    cin >> m;
+    float constb = 0;
+    float *b = new float[m + 1];
+    cin >> constb;
+    for (int i = 0; i < m + 1; i++)
+    {
+        cin >> b[i];
+    }
+    float constc = 0;
+    float *c;
+    cout << "sum of two polynomials : ";
+    c = add(a, b, n + 1, m + 1);
+    constc = consta + constb;
+    print(c, max(n + 1, m + 1), constc);
+    cout << "difference of two polynomials : ";
+    constc = constb - consta;
+    c = subtract(b, a, m + 1, n + 1);
+    print(c, max(n + 1, m + 1), constc);
+    cout << "product of two polynomials : ";
+    constc = consta * constb;
+    c = multiply(a, b, n + 1, m + 1);
+    print(c, n + 1 + m, constc);
+    delete[] a;
+    delete[] b;
+    delete[] c;
 }
