731c315bbc32e70d8ac3476899eb6432be1f8060
ziad-walid <ziadwalid320@gmail.com>
2025-05-06 22:43:49 +0300

    Revert "organize"
    This reverts commit d2c0bf86506ced08eae7d0382a39f96ec202e8a2.

--- Assignment 1/problem_five/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "iostream"
-#include "StatisticalCalculation.h"
-using namespace std;
-int main(){
-    StatisticalCalculation<double> statisticalCalculation(1);
-    statisticalCalculation.inputData();
-    statisticalCalculation.statisticsMenu();
-    return 0;
-}
